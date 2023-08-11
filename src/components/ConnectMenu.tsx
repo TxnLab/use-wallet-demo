057d28b80e70943426675fc5888f673db682d8e2
Doug Richar <drichar@gmail.com>
2023-08-11 06:17:50 -0400

    refactor: rm MenuHeader, put JSX in ConnectMenu

--- src/components/ConnectMenu.tsx
+++ src/components/ConnectMenu.tsx
@@ -1,9 +1,10 @@
 import { Dialog, Transition } from '@headlessui/react'
+import { XMarkIcon } from '@heroicons/react/24/outline'
+import { useWallet } from '@txnlab/use-wallet'
 import { Fragment, useState } from 'react'
 import ConnectButton from '@/components/ConnectButton'
-import MenuHeader from '@/components/MenuHeader'
 import Provider from '@/components/Provider'
-import { useWallet } from '@txnlab/use-wallet'
+import UserThumbnail from '@/components/UserThumbnail'
 
 export default function ConnectMenu() {
   const [open, setOpen] = useState(false)
@@ -60,13 +61,45 @@ export default function ConnectMenu() {
                   leaveTo="translate-x-full"
                 >
                   <Dialog.Panel className="pointer-events-auto w-screen max-w-md">
-                    <div className="flex h-full flex-col overflow-y-scroll bg-zinc-900 shadow-xl">
-                      <MenuHeader
-                        activeAddress={activeAddress}
-                        setOpen={setOpen}
-                        title="Connect a wallet to get started"
-                        subtitle="Select the wallet provider you would like to connect to from the options below."
-                      />
+                    <div
+                      className="flex h-full flex-col overflow-y-scroll bg-zinc-900 shadow-xl"
+                      data-testid="connect-menu"
+                    >
+                      <div className="bg-teal-700 px-4 py-6 sm:px-6">
+                        <div className="flex items-start justify-between">
+                          <Dialog.Title className="text-base font-semibold leading-6 text-white">
+                            {!activeAddress ? (
+                              'Connect a wallet to get started'
+                            ) : (
+                              <UserThumbnail address={activeAddress} />
+                            )}
+                          </Dialog.Title>
+                          <div className="ml-3 flex h-7 items-center">
+                            <button
+                              type="button"
+                              className="relative rounded-md bg-teal-700 text-teal-200 hover:text-white focus:outline-none focus:ring-2 focus:ring-white"
+                              onClick={() => setOpen(false)}
+                              data-testid="close-menu"
+                            >
+                              <span className="absolute -inset-2.5" />
+                              <span className="sr-only">Close panel</span>
+                              <XMarkIcon
+                                className="h-6 w-6"
+                                aria-hidden="true"
+                              />
+                            </button>
+                          </div>
+                        </div>
+
+                        {!activeAddress && (
+                          <div className="mt-1">
+                            <p className="text-sm text-teal-300">
+                              Select the wallet provider you would like to
+                              connect to from the options below.
+                            </p>
+                          </div>
+                        )}
+                      </div>
                       <div className="relative flex-1 py-6 sm:px-6">
                         <div className="flex flex-col gap-6 sm:gap-7">
                           {renderProviders()}
