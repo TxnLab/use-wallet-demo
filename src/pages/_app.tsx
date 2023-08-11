39e9683f0c121de37eecb3aa0a963a8f9de0b827
Doug Richar <drichar@gmail.com>
2023-08-11 06:36:21 -0400

    feat: dynamic imports of provider dependencies

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,3 @@
-import { DeflyWalletConnect } from '@blockshake/defly-connect'
-import { DaffiWalletConnect } from '@daffiwallet/connect'
-import { PeraWalletConnect } from '@perawallet/connect'
-import { WalletConnectModalSign } from '@walletconnect/modal-sign-html'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import {
   PROVIDER_ID,
@@ -13,15 +9,40 @@ import '@/styles/globals.css'
 
 const queryClient = new QueryClient()
 
+const getDynamicDeflyWalletConnect = async () => {
+  const DeflyWalletConnect = (await import('@blockshake/defly-connect'))
+    .DeflyWalletConnect
+  return DeflyWalletConnect
+}
+
+const getDynamicPeraWalletConnect = async () => {
+  const PeraWalletConnect = (await import('@perawallet/connect'))
+    .PeraWalletConnect
+  return PeraWalletConnect
+}
+
+const getDynamicDaffiWalletConnect = async () => {
+  const DaffiWalletConnect = (await import('@daffiwallet/connect'))
+    .DaffiWalletConnect
+  return DaffiWalletConnect
+}
+
+const getDynamicWalletConnectModalSign = async () => {
+  const WalletConnectModalSign = (
+    await import('@walletconnect/modal-sign-html')
+  ).WalletConnectModalSign
+  return WalletConnectModalSign
+}
+
 export default function App({ Component, pageProps }: AppProps) {
   const providers = useInitializeProviders({
     providers: [
-      { id: PROVIDER_ID.DEFLY, clientStatic: DeflyWalletConnect },
-      { id: PROVIDER_ID.PERA, clientStatic: PeraWalletConnect },
-      { id: PROVIDER_ID.DAFFI, clientStatic: DaffiWalletConnect },
+      { id: PROVIDER_ID.DEFLY, getDynamicClient: getDynamicDeflyWalletConnect },
+      { id: PROVIDER_ID.PERA, getDynamicClient: getDynamicPeraWalletConnect },
+      { id: PROVIDER_ID.DAFFI, getDynamicClient: getDynamicDaffiWalletConnect },
       {
         id: PROVIDER_ID.WALLETCONNECT,
-        clientStatic: WalletConnectModalSign,
+        getDynamicClient: getDynamicWalletConnectModalSign,
         clientOptions: {
           projectId: '74dec24df599bff521ad8c630dfe9f0b',
           metadata: {
